8bebcb6c498d501ebc987b7921750e3d502495d8
AshutoshKumar13 <ashutosh.kumar@getvisitapp.com>
2026-01-17 14:22:14 +0530

    chore: fix deployment config - remove tracked dist, fix outDir, update gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react()],
